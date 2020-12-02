4a6d840664d15fc1d2ba452886afb56e5421204c
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-12-02 23:49:51 +0600

    redux procedure

--- src/store/index.js
+++ src/store/index.js
@@ -4,3 +4,26 @@ import rootReducer from './reducers/rootReducer';
 const store = createStore(rootReducer);
 
 export default store;
+
+// The procedure of redux
+// Redux
+    // Action
+    // Reducers
+
+// React components
+    // Chiuld Components
+        // Chiuld Components
+
+// React-Redux connect
+    // Provider component
+        // Store
+        // App component
+
+// For getting data from redux
+// mapStateToProps(state) return object
+
+// For dispatching some action
+// mapDispatchToProps(dispatch) return object
+
+// connect(mapStateToProps)(component)
+// connect(null, mapDispatchToProps)(component)
